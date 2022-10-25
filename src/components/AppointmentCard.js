48b7542a7ecf4e7ddf07f7d3331d9f96cad74df1
Daniel Boley <danboley1@gmail.com>
2022-10-25 15:43:44 -0400

    edited appointment and appointmentcard components to access nested data from fetch

--- src/components/AppointmentCard.js
+++ src/components/AppointmentCard.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-function AppointmentCard({date, time, notes, location, tutor_id, student_id}) {
+function AppointmentCard({date, time, notes, location, tutor, student}) {
+
     return (
         <div>
             <h1>{date}, {time}</h1>
             <h2>{notes}</h2>
             <h2>{location}</h2>
-            <h2>{tutor_id}, {student_id}</h2>
+            
+            <h2>Tutor: {tutor.first_name} {tutor.last_name}</h2>
+            <h2>Student: {student.first_name} {student.last_name}</h2>
             <a>Click to change or cancel appointment.</a>
             <br></br>
         </div>
