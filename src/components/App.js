73282bf2b187caeb927e97d9e17e0600d28d9107
Daniel Boley <danboley1@gmail.com>
2022-10-26 17:59:19 -0400

    fixed appt patch

--- src/components/App.js
+++ src/components/App.js
@@ -1,94 +1,114 @@
-import React, { useState, useEffect } from 'react';
-import NavBar from './NavBar';
-import Home from './Home';
-import Appointment from './Appointment';
-import Student from './Student';
-import Tutor from './Tutor';
-import AppointmentForm from './AppointmentForm';
-import { Switch, Route } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import Appointment from "./Appointment";
+import Student from "./Student";
+import Tutor from "./Tutor";
+import AppointmentForm from "./AppointmentForm";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   // appointments state
-  const [appointments, setAppointments] = useState([])
+  const [appointments, setAppointments] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:9292/appointments')
-    .then((r) => r.json())
-    .then((appointments) => setAppointments(appointments))
-    }, [])
+    fetch("http://localhost:9292/appointments")
+      .then((r) => r.json())
+      .then((appointments) => setAppointments(appointments));
+  }, []);
 
   function addAppointment(newAppointment) {
-    setAppointments(appointments => [...appointments, newAppointment])
-    }
+    setAppointments((appointments) => [...appointments, newAppointment]);
+  }
 
-  function updateAppointment(updatedAppointment){
+  function updateAppointment(updatedAppointment) {
     const updatedAppointments = appointments.map((appointment) => {
       if (appointment.id === updatedAppointment.id) {
         return updatedAppointment;
-      }
-      else {
+      } else {
         return appointment;
       }
-    })
+    });
     setAppointments(updatedAppointments);
-    console.log(updatedAppointments);
   }
 
   function removeAppointment(id) {
-    const removeSelectedAppointment = appointments.filter((appointment) => appointment.id !== id);
+    const removeSelectedAppointment = appointments.filter(
+      (appointment) => appointment.id !== id
+    );
     setAppointments(removeSelectedAppointment);
   }
 
-      // student state
-      const [students, setStudents] = useState([]);
+  // student state
+  const [students, setStudents] = useState([]);
 
-      useEffect(() => {
-        fetch("http://localhost:9292/students")
-          .then((r) => r.json())
-          .then((students) => setStudents(students));
-      }, []);
+  useEffect(() => {
+    fetch("http://localhost:9292/students")
+      .then((r) => r.json())
+      .then((students) => setStudents(students));
+  }, []);
 
-      function removeStudent(id) {
-        const removeStudentCard = students.filter((student) => student.id !== id);
-        setStudents(removeStudentCard);
-      }
+  function removeStudent(id) {
+    const removeStudentCard = students.filter((student) => student.id !== id);
+    setStudents(removeStudentCard);
+  }
 
-      //tutor state
-      const [tutors, setTutors] = useState([])
+  //tutor state
+  const [tutors, setTutors] = useState([]);
 
-      useEffect(() => {
-        fetch('http://localhost:9292/tutors')
-        .then((r) => r.json())
-        .then((tutors) => setTutors(tutors))
-        }, [])
+  useEffect(() => {
+    fetch("http://localhost:9292/tutors")
+      .then((r) => r.json())
+      .then((tutors) => setTutors(tutors));
+  }, []);
 
   return (
-    <div className='paper'>
+    <div className="paper">
       <header>
-        <NavBar/>
+        <NavBar />
         <br></br>
-        <h1 className='header'>Tutors + Appointments</h1>
+        <h1 className="header">Tutors + Appointments</h1>
         <br></br>
         <Switch>
           <Route exact path="/tutors">
-            <Tutor tutors={tutors} setTutors={setTutors}/>
+            <Tutor tutors={tutors} setTutors={setTutors} />
           </Route>
           <Route exact path="/appointments">
-            <Appointment appointments={appointments} updateAppointment={updateAppointment} addAppointment={addAppointment} tutors={tutors} students={students} removeAppointment={removeAppointment}/>
+            <Appointment
+              appointments={appointments}
+              updateAppointment={updateAppointment}
+              addAppointment={addAppointment}
+              tutors={tutors}
+              students={students}
+              removeAppointment={removeAppointment}
+            />
           </Route>
           <Route exact path="/students">
-            <Student removeStudent={removeStudent} students={students} setStudents={setStudents}/>
+            <Student
+              removeStudent={removeStudent}
+              students={students}
+              setStudents={setStudents}
+            />
           </Route>
           <Route exact path="/newappointment">
-            <AppointmentForm appointments={appointments} setAppointments={setAppointments} addAppointment={addAppointment} tutors={tutors} students={students}/>
+            <AppointmentForm
+              appointments={appointments}
+              setAppointments={setAppointments}
+              addAppointment={addAppointment}
+              tutors={tutors}
+              students={students}
+            />
           </Route>
           <Route exact path="/">
             <Home />
           </Route>
           <Route path="*">
-            <h1>Uh oh! Looks like you're not in class and don't have a hall pass. Make an appointment with one of our tutors to get back on track! </h1>
+            <h1>
+              Uh oh! Looks like you're not in class and don't have a hall pass.
+              Make an appointment with one of our tutors to get back on track!{" "}
+            </h1>
           </Route>
-          </Switch>
+        </Switch>
       </header>
     </div>
   );
